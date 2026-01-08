091266d41003c2b45feab90c037b9a3069a1d95f
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2026-01-08 10:11:17 -0500

    fix: MacOS version mismatch

--- src/main.cpp
+++ src/main.cpp
@@ -660,6 +660,17 @@ int main(int argc, char* argv[])
 {
     if (!glfwInit()) return -1;
 
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#else
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+
     const int windowHeight = 900;
     const int windowWidth = 600;
     GLFWwindow* window = glfwCreateWindow(windowHeight, windowWidth, "Sine Installer", nullptr, nullptr);
