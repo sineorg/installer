22ec15e94d8dfb7ac6fe05efb65b712c52da99f2
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2026-01-31 12:54:24 -0500

    fix: Use sudo if failed pkexec

--- src/main.cpp
+++ src/main.cpp
@@ -485,7 +485,7 @@ bool requestAdmin(
         return true;
 
     // If pkexec failed with agent not found, fallback to sudo in terminal
-    if (WIFEXITED(result) && WEXITSTATUS(result) != 1)
+    if (WIFEXITED(result) && WEXITSTATUS(result) != 0)
     {
         // Use x-terminal-emulator or gnome-terminal or konsole
         const char* terminal = std::getenv("TERMINAL");
