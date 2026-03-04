64eb26261ab18084cf35d79de74be8a44b129706
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2026-03-04 11:01:37 -0500

    fix: Use a separate log file for admin processes

--- updater.bat
+++ updater.bat
@@ -150,18 +150,18 @@ if not defined extPath (
 if not defined isLibrewolf set "isLibrewolf=false"
 if not defined isNested set "isNested=true"
 
-echo Install bootloader? %installBoot%. Is Librewolf? !isLibrewolf!. >> "installer.log"
+echo Install bootloader? %installBoot%. Is Librewolf? !isLibrewolf!. >> "bootloader.log"
 
 if "%installBoot%"=="true" if "!isLibrewolf!"=="false" (
-    echo Triggering bootloader events >> "installer.log"
+    echo Triggering bootloader events >> "bootloader.log"
     if defined uninstall (
-        echo Triggering bootloader uninstallation >> "installer.log"
-        del "!extPath!\config.js" 2>>installer.log
-        del "!extPath!\defaults\pref\config-prefs.js" 2>>installer.log
+        echo Triggering bootloader uninstallation >> "bootloader.log"
+        del "!extPath!\config.js" 2>>bootloader.log
+        del "!extPath!\defaults\pref\config-prefs.js" 2>>bootloader.log
     ) else (
-        echo Triggering bootloader installation >> "installer.log"
+        echo Triggering bootloader installation >> "bootloader.log"
         "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -Command ^
-            "Expand-Archive -Force 'program.zip' '!extPath!'" 2>>installer.log
+            "Expand-Archive -Force 'program.zip' '!extPath!'" 2>>bootloader.log
 
         if !isNested!=="true" (
             exit
@@ -174,4 +174,5 @@ del program.zip 2>>installer.log
 if exist "%chromeFolder%\update" (
     del "%chromeFolder%\update" 2>>installer.log
 )
+
 
